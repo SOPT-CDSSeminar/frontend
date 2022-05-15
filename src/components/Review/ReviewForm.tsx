3213df23e612ffa49e76cf858f4c8f5aa6cda65c
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-05-15 19:53:03 +0900

    [etc] 메모 작성

--- src/components/Review/ReviewForm.tsx
+++ src/components/Review/ReviewForm.tsx
@@ -1,3 +1,12 @@
+/*
+마지막 편집자: 22-05-15 joohaem
+변경사항 및 참고:
+  - 서히 파트
+  - 열쩡킹짱서히 잘 할거지 ?? ~#~
+  - 내에서 자유롭게 컴포넌트 분리해도 되고 도입하고 싶은 거 있으면 언제든지 환영 ~~
+  - 화이팅텡!
+*/
+
 export default function ReviewForm() {
   return <div>ReviewForm</div>;
 }
