99b82133575f9d46098921255bf551cf0a857aab
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-05-15 20:06:40 +0900

    [feat] add styled-reset

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { reset } from "styled-reset";
 
 import App from "./App";
 import theme from "./style/theme";
 
 export const GlobalStyle = createGlobalStyle`
-*{
-  font-family: 'Noto Sans KR', sans-serif;
-}
+  ${reset}
+
+  * {
+    font-family: 'Noto Sans KR', sans-serif;
+  } 
+
+  html {
+    font-size: 62.5%;
+  }
 `;
 
 ReactDOM.render(
