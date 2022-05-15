1160ae73b3188b348198d22661056f4df98be6ba
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-05-15 19:59:58 +0900

    [feat] setting theme

--- src/pages/Review.tsx
+++ src/pages/Review.tsx
@@ -20,4 +20,8 @@ const StMainWrapper = styled.main`
   max-width: 37.5rem;
 
   margin: 0 auto;
+
+  /* 아래는 예시코드
+  color: ${({ theme }) => theme.colors.ohou_gray01};
+  ${({ theme }) => theme.fonts.ohou_h1}; */
 `;
