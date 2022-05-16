88f0004bbbd4f9071825067d03bda8e29dfa5422
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-05-16 14:39:54 +0900

    [feat] export svg files

---
+++ src/asset/icon/index.ts
@@ -0,0 +1,20 @@
+/*
+마지막 편집자: 22-05-16 joohaem
+변경사항 및 참고:
+  - 1. 위 양식대로 추가한다
+  - 2. 컴포넌트에서 <IcSearch /> 로 불러와서 사용한다 !!!!
+*/
+
+export { ReactComponent as IcCart } from "./MyPage/cartIcon.svg";
+export { ReactComponent as IcApple } from "./MyPage/footer/appleIcon.svg";
+export { ReactComponent as IcBlog } from "./MyPage/footer/blogIcon.svg";
+export { ReactComponent as IcDnv } from "./MyPage/footer/dnvIcon.svg";
+export { ReactComponent as IcFacebook } from "./MyPage/footer/facebookIcon.svg";
+export { ReactComponent as IcGoogleplay } from "./MyPage/footer/googleplayIcon.svg";
+export { ReactComponent as IcInstargram } from "./MyPage/footer/instargramIcon.svg";
+export { ReactComponent as IcIsms } from "./MyPage/footer/ismsIcon.svg";
+export { ReactComponent as IcKakaostory } from "./MyPage/footer/kakaostoryIcon.svg";
+export { ReactComponent as IcNaver } from "./MyPage/footer/naverIcon.svg";
+export { ReactComponent as IcHamburger } from "./MyPage/hamburgerIcon.svg";
+export { ReactComponent as IcLogo } from "./MyPage/logoIcon.svg";
+export { ReactComponent as IcSearch } from "./MyPage/searchIcon.svg";
