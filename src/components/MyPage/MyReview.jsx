858407617e8ac7e2260f4499a6fdb2ecd33dd63e
songah119 <skutcm119@gmail.com>
2022-05-18 01:47:40 +0900

    [feat] 기본틀, 서버에서 데이터 받아오기

--- src/components/MyPage/MyReview.jsx
+++ src/components/MyPage/MyReview.jsx
@@ -1,5 +1,4 @@
 /*
-마지막 편집자: 22-05-15 joohaem
 변경사항 및 참고:
   - 송아 파트
   - 베스트순 최신순 ~~~ 상품 정보 리스트 나열해주기!
@@ -13,7 +12,73 @@
 
   - 화이ㅣ팅 ~~
 */
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 
 export default function MyReview() {
-  return <div>MyReview</div>;
+  const [products, setProducts] = useState([]);
+
+  async function getDataList() {
+    const result = await axios.get("http://localhost:4000/data");
+    console.log("통신 결과: ", result);
+    setProducts(result.data);
+  }
+
+  useEffect(() => {
+    getDataList();
+  }, []);
+
+  return (
+    <>
+      <StBtnWrapper>
+        <StBtnBestOrder>베스트순</StBtnBestOrder>
+        <StBtnLatestOrder>최신순</StBtnLatestOrder>
+      </StBtnWrapper>
+      {products.map((content, idx) => {
+        const { name, place, size, nickname, date, platform, image, review } = content;
+        return (
+          <StProductCard key={name}>
+            <StProductTitle>
+              [ {place} ] {name}
+            </StProductTitle>
+            {size}
+            <StReviewInfo>
+              {nickname} | {date} | {platform} 리뷰
+            </StReviewInfo>
+            <StReviewText>{review}</StReviewText>
+            <img src={image} alt="페브릭 포스터" />
+            <button>수정</button>
+          </StProductCard>
+        );
+      })}
+    </>
+  );
 }
+
+const StBtnWrapper = styled.section`
+  display: flex;
+`;
+
+const StBtnBestOrder = styled.p`
+  position: absolute;
+`;
+
+const StBtnLatestOrder = styled.p`
+  position: absolute;
+`;
+
+const StProductCard = styled.article`
+  position: absolute;
+`;
+
+const StProductTitle = styled.p`
+  position: absolute;
+`;
+
+const StReviewInfo = styled.p`
+  position: absolute;
+`;
+const StReviewText = styled.p`
+  position: absolute;
+`;
