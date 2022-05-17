7c5b6a87a573f7fbf92c53bd215b269be5df6334
seohee0112 <ghdtj7@naver.com>
2022-05-17 15:00:50 +0900

    [ feat ] 사진첨부에 필요한 아이콘 모듈 구현

---
+++ src/asset/icon/index.ts
@@ -0,0 +1,4 @@
+export { ReactComponent as ICAttachPhoto } from "./Review/attach_photo.svg";
+export { ReactComponent as ICAttachPhotoBtn } from "./Review/btn_attach_photo.svg";
+export { ReactComponent as ICDeleteBtn } from "./Review/btn_delete.svg";
+export { ReactComponent as ICPhotoReviewBubble } from "./Review/photoreview_bubble.svg";
