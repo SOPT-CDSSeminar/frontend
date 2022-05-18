28044d2c19f00ab574fa5cc4f47f9f7109486c14
songah119 <skutcm119@gmail.com>
2022-05-18 16:58:32 +0900

    [code review] 절대 경로 삭제..!ㅎ

--- src/components/MyPage/MyReview.jsx
+++ src/components/MyPage/MyReview.jsx
@@ -21,7 +21,7 @@ export default function MyReview() {
 
   async function getDataList() {
     const result = await axios.get("http://localhost:4000/data");
-    console.log("통신 결과: ", result);
+    //console.log("통신 결과: ", result);
     setProducts(result.data);
   }
 
@@ -31,8 +31,10 @@ export default function MyReview() {
 
   return (
     <>
-      <StBtnBestOrder>베스트순</StBtnBestOrder>
-      <StBtnLatestOrder>최신순</StBtnLatestOrder>
+      <StBtnWrapper>
+        <StBtnBestOrder>베스트순</StBtnBestOrder>
+        <StBtnLatestOrder>최신순</StBtnLatestOrder>
+      </StBtnWrapper>
       {products.map((content, idx) => {
         const { name, place, size, nickname, date, platform, image, review } = content;
         return (
@@ -45,8 +47,8 @@ export default function MyReview() {
               {nickname} | {date} | {platform} 리뷰
             </StReviewInfo>
             <StReviewText>{review}</StReviewText>
-            <StReviewImg src={image} alt="페브릭 포스터" />
             <StBtnEdit>수정</StBtnEdit>
+            <StReviewImg src={image} alt="페브릭 포스터" />
           </StProductCard>
         );
       })}
@@ -54,20 +56,19 @@ export default function MyReview() {
   );
 }
 
-const StBtnBestOrder = styled.p`
-  position: absolute;
-  left: 2.4rem;
-  top: 24.2rem;
+const StBtnWrapper = styled.section`
+  display: flex;
 
+  margin-bottom: 7.6rem;
+  margin-top: 5.9 rem;
+`;
+
+const StBtnBestOrder = styled.p`
   font-family: ${({ theme }) => theme.fonts.ohou_h3};
   color: ${({ theme }) => theme.colors.ohou_gray06};
 `;
 
 const StBtnLatestOrder = styled.p`
-  position: absolute;
-  left: 10.7rem;
-  top: 24.2rem;
-
   font-family: ${({ theme }) => theme.fonts.ohou_h3};
   color: ${({ theme }) => theme.colors.ohou_skyblue};
 
@@ -75,15 +76,10 @@ const StBtnLatestOrder = styled.p`
 `;
 
 const StProductCard = styled.article`
-  position: absolute;
+  display: block;
 `;
 
 const StProductTitle = styled.p`
-  position: absolute;
-  width: 34.7rem;
-  height: 4rem;
-  left: 1.4rem;
-  top: 33.7rem;
   margin-bottom: 0.4rem;
 
   font-family: ${({ theme }) => theme.fonts.ohou_h2};
@@ -92,27 +88,20 @@ const StProductTitle = styled.p`
 `;
 
 const StProductSize = styled.p`
-  position: absolute;
   width: 6.8rem;
   height: 1.7rem;
-  left: 1.4rem;
-  top: 38.1rem;
+
   margin-bottom: 2.7rem;
 
   font-family: ${({ theme }) => theme.fonts.ohou_b1};
   color: ${({ theme }) => theme.colors.ohou_gray06};
-
   letter-spacing: -0.04em;
-
-  color: #292929;
 `;
 
 const StReviewInfo = styled.p`
-  position: absolute;
   width: 20.7rem;
   height: 1.7rem;
-  left: 1.4rem;
-  top: 42.5rem;
+
   margin-bottom: 1.2rem;
 
   font-family: ${({ theme }) => theme.fonts.ohou_b3};
@@ -120,33 +109,27 @@ const StReviewInfo = styled.p`
 `;
 
 const StReviewText = styled.p`
-  position: absolute;
-  width: 34.7rem;
-  height: 4.2rem;
-  left: 1.4rem;
-  top: 45.4rem;
-  margin-bottom: 2.4rem;
-
   font-family: ${({ theme }) => theme.fonts.ohou_b2};
   color: ${({ theme }) => theme.colors.ohou_gray06};
 `;
 
 const StReviewImg = styled.img`
-  position: absolute;
   width: 9.1rem;
   height: 9.1rem;
-  left: 1.4rem;
-  top: 52rem;
+
+  margin-top: 2.4rem;
 
   border-radius: 0.4rem;
 `;
 
 const StBtnEdit = styled.button`
+  width: 2.3rem;
+  height: 2.1rem;
+
+  margin-top: 0.8rem;
+  margin-left: 32.5rem;
+
   position: absolute;
-  width: 4.8rem;
-  height: 4.8rem;
-  left: 31.3rem;
-  top: 49.1rem;
 
   font-family: "Noto Sans KR";
   font-style: normal;
