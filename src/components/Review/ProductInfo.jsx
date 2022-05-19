70f1842318cc1c59e93b85c1ef77753df706da31
NYeonK <skdus728@gmail.com>
2022-05-19 16:00:35 +0900

    [feat] 상품 정보 블럭 css

--- src/components/Review/ProductInfo.jsx
+++ src/components/Review/ProductInfo.jsx
@@ -17,7 +17,7 @@ export default function ProductInfo() {
       <StProductDetail>
         <StDetailStore>멜팅스튜디오</StDetailStore>
         <StDetailTitle>느긋한 오후의 기록, 쉬폰 패브릭 포스터 / 73 Boston #2 / 142-225cm</StDetailTitle>
-        <StDetailSize>142-225cm</StDetailSize>
+        <StDetailSize>142 - 225cm</StDetailSize>
       </StProductDetail>
     </StProduct>
   );
@@ -28,8 +28,29 @@ const StProduct = styled.div`
   margin: 1.8rem 1.4rem;
 `;
 const StProductImg = styled.div``;
-const StProductDetail = styled.div``;
+const StProductDetail = styled.div`
+  margin-left: 1.1em;
+  margin-top: 0.9em;
+`;
 
-const StDetailStore = styled.p``;
-const StDetailTitle = styled.p``;
-const StDetailSize = styled.p``;
+const StDetailStore = styled.p`
+  font-family: "Noto Sans KR";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1em;
+  line-height: 1.4em;
+  letter-spacing: -0.04em;
+  color: ${({ theme }) => theme.colors.ohou_gray04};
+  margin-bottom: 0.2em;
+`;
+const StDetailTitle = styled.p`
+  ${({ theme }) => theme.fonts.ohou_b1};
+  color: ${({ theme }) => theme.colors.ohou_gray05};
+  font-family: "Noto Sans KR";
+  margin-bottom: 0.4em;
+`;
+const StDetailSize = styled.p`
+  ${({ theme }) => theme.fonts.ohou_b4};
+  color: ${({ theme }) => theme.colors.ohou_gray04};
+  font-family: "Noto Sans KR";
+`;
