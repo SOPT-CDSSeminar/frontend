5c6df1a8f2344254f6027de07a3f338e675ea040
seohee0112 <ghdtj7@naver.com>
2022-06-03 16:26:26 +0900

    [ remove ] 프록시 삭제

--- src/utils/lib/index.ts
+++ src/utils/lib/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const reqAPI = axios.create({
-  baseURL: "http://cors-anywhere.herokuapp.com/http://3.90.162.48:8000",
+  baseURL: "http://3.90.162.48:8000",
   headers: {
     "Content-type": "application/json",
   },
