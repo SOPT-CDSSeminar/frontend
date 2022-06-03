89fc2f438fc528721f4f4ac5c8329605fe5dfd13
songah119 <skutcm119@gmail.com>
2022-06-03 20:30:10 +0900

    [feat] 환경변수 설정

--- src/utils/lib/index.ts
+++ src/utils/lib/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const reqAPI = axios.create({
-  baseURL: "http://3.90.162.48:8000",
+  baseURL: `${process.env.REACT_APP_IP}`,
   headers: {
     "Content-type": "application/json",
   },
